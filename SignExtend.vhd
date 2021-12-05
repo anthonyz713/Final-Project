3bb32816ad01e1d4673d84ad1597c8f3df5924b1
anthonyz713 <78340967+anthonyz713@users.noreply.github.com>
2021-12-05 00:57:30 -0800

    Change SignExtend input & output name

--- SignExtend.vhd
+++ SignExtend.vhd
@@ -4,22 +4,22 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity SignExtend is
-   port(instruction16: in std_logic_vector(15 downto 0);
-        instruction32: out std_logic_vector(31 downto 0));
+   port(immediate16: in std_logic_vector(15 downto 0);
+        immediate32: out std_logic_vector(31 downto 0));
 end SignExtend;
 
 architecture SignExtendBehav of SignExtend is
 begin
-   process(instruction16)
+   process(immediate16)
    begin
-      if to_integer(signed(instruction16)) = 0 then
-         instruction32 <= (others => '0');
-      elsif to_integer(signed(instruction16)) > 0 then -- positive
-         instruction32(31 downto 16) <= (others => '0');
-         instruction32(15 downto 0) <= instruction16;
+      if to_integer(signed(immediate16)) = 0 then
+         immediate32 <= (others => '0');
+      elsif to_integer(signed(immediate16)) > 0 then -- positive
+         immediate32(31 downto 16) <= (others => '0');
+         immediate32(15 downto 0) <= immediate16;
       else -- negative
-         instruction32(31 downto 16) <= (others => '1');
-         instruction32(15 downto 0) <= instruction16;
+         immediate32(31 downto 16) <= (others => '1');
+         immediate32(15 downto 0) <= immediate16;
       end if;
    end process;
 end;
