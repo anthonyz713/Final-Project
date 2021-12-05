84bc7c8e07dd6026e3b88da323c807854dff90a1
anthonyz713 <78340967+anthonyz713@users.noreply.github.com>
2021-12-05 00:56:06 -0800

    Update InstructionMemory to read all 4 bytes in one cc

--- InstructionMemory.vhd
+++ InstructionMemory.vhd
@@ -1,20 +1,15 @@
 -- Group Members: Michelle Gamba, Jesus Ruelas-Perez, Anthony Zhang
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity InstructionMemory is
-   port(Clk, MemR: in std_logic;
+   port(clk, MemR: in std_logic;
         PC: inout std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
-        Dout: out std_logic_vector(7 downto 0);
         IR: out std_logic_vector(31 downto 0));
 end InstructionMemory;
 
 architecture InstructionMemoryBehav of InstructionMemory is
-signal Addr: std_logic_vector(4 downto 0) := PC(4 downto 0);
-signal byteNum: Integer := 1; -- 1 = MSB, 4 = LSB
-
 type ROM_array is array (0 to 19) of std_logic_vector(7 downto 0);
 constant ROM: ROM_array := (
    -- add $t0, $t1, $t2
@@ -49,31 +44,15 @@ constant ROM: ROM_array := (
    );
 
 begin
-   process(Clk)
+   process(clk)
    begin
-      if rising_edge(Clk) and MemR = '1' then
-         -- Dout = memory[Addr]
-         Dout <= ROM(to_integer(unsigned(Addr)));
+      if rising_edge(clk) and MemR = '1' then
+         IR(31 downto 24) <= ROM(to_integer(unsigned(PC)));
+         IR(23 downto 16) <= ROM(to_integer(unsigned(PC)) + 1);
+         IR(15 downto 8) <= ROM(to_integer(unsigned(PC)) + 2);
+         IR(7 downto 0) <= ROM(to_integer(unsigned(PC)) + 3);
 
-         -- IR[byteNum] = memory[Addr]
-         -- update byteNum for next byte
-         if byteNum = 1 then
-            IR(31 downto 24) <= ROM(to_integer(unsigned(Addr)));
-            byteNum <= 2;
-         elsif byteNum = 2 then
-            IR(23 downto 16) <= ROM(to_integer(unsigned(Addr)));
-            byteNum <= 3;
-         elsif byteNum = 3 then
-            IR(15 downto 8) <= ROM(to_integer(unsigned(Addr)));
-            byteNum <= 4;
-         elsif byteNum = 4 then
-            IR(7 downto 0) <= ROM(to_integer(unsigned(Addr)));
-            byteNum <= 1;
-         end if;
-         
-         -- Addr = Addr + 1
-         Addr <= std_logic_vector(unsigned(Addr) + 1);
-         PC(4 downto 0) <= std_logic_vector(unsigned(Addr) + 1);
+         PC <= std_logic_vector(unsigned(PC) + 4);
       end if;
    end process;
 end;
